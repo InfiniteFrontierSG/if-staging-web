fece5929925f9dfddbd553afb4a2bca9dedeabe1
Wilson <wilson@policypal.com>
2020-05-28 01:37:37 +0800

    Update Nextjs

---
+++ _next/static/xQWeRxMjEWU7CAij4bkD_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/nlevel-calculator":[a,"static\u002Fcss\u002Facf4e6da848d3827ffd3.css"]}}("static\u002Fchunks\u002F75792f2dbf70b0920e3bae4e5082380efd06cf90.d59032038c4685e772c6.js","static\u002Fcss\u002F8456f44aba54a6fb0aad.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
